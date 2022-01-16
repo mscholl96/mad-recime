aff1ef44809626305dbdea61682ac3aa03691a7f
Maximilian Scholl <maximilian@schollies.de>
2022-01-16 19:40:25 +0100

    #12 Use pandas for data representation, ignore invalid, preprocess fractions

---
+++ data/recipe1M/parser/recipe.py
@@ -0,0 +1,49 @@
+
+
+from quantulum3 import parser as qparse
+import pandas as pd
+       
+class Recipe:
+    """Class to represent a single recipe"""
+    def __init__(self, id):
+        self.title = ""
+        self.id = id
+        self.ingredients = pd.DataFrame(columns=['amount', 'unit', 'ingredient'])
+        self.instructions = pd.Series(dtype=str)
+
+    def __str__(self):
+        return f"ID: {self.id} \nTitle: {self.title} \nIngredients: {str(self.ingredients)} \nInstructions {self.instructions}"
+    
+    def __repr__(self):
+        return str(self)
+
+    def parse_ingredients(self, raw_ingredients):
+        # Ignore if more than 20
+        if len(raw_ingredients) > 20:
+            return False
+        for elem in raw_ingredients:
+            self.ingredients = self.ingredients.append({'amount': 1, 'unit':'', 'ingredient': elem['text']}, ignore_index=True)
+        return True
+
+    def parse_instructions(self, raw_instructions):
+        # Ignore more than 30
+        if len(raw_instructions) > 30:
+            return False
+        for elem in raw_instructions:
+            self.instructions = self.instructions.append(pd.Series(elem['text'], dtype=str), ignore_index=True)
+        return True
+    
+    def get_ingredient_amounts(self, raw_ingredients):
+        for _, ingredient in self.ingredients.iterrows():
+            # Find corresponding ingredient text in raw recipe
+            for raw in raw_ingredients:
+                text = raw['text'].lower()
+                if ingredient.ingredient in text:
+                    quants = qparse.parse(text)
+                    if len(quants) != 0:
+                        ent = quants[0].unit.entity
+                        ingredient.unit = "" if quants[0].unit.name == 'dimensionless' else quants[0].unit.name
+                        ingredient.amount = quants[0].value
+
+                        if not ent.name in 'dimensionless, volume, mass':
+                            print(f"Determined entity: {ent.name}, from unit: {ingredient.unit} of text: {text}")
