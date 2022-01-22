0a684e8901c85ac3f8b8f53fb4869ce8adc5ed3b
Maximilian Scholl <maximilian@schollies.de>
2022-01-22 17:34:56 +0100

    #12 Use lists instead of frames and optimzed loops

--- data/recipe1M/parser/recipe.py
+++ data/recipe1M/parser/recipe.py
@@ -8,8 +8,8 @@ class Recipe:
     def __init__(self, id):
         self.title = ""
         self.id = id
-        self.ingredients = pd.DataFrame(columns=['amount', 'unit', 'ingredient'])
-        self.instructions = pd.Series(dtype=str)
+        self.ingredients = []
+        self.instructions = []
 
     def __str__(self):
         return f"ID: {self.id} \nTitle: {self.title} \nIngredients: {str(self.ingredients)} \nInstructions {self.instructions}"
@@ -17,48 +17,38 @@ class Recipe:
     def __repr__(self):
         return str(self)
 
+    def to_dict(self):
+        return {'id': self.id, 'title': self.title, 'ingredients': pd.DataFrame(self.ingredients), 'instructions': pd.Series(self.instructions)}
+
     def parse_ingredients(self, raw_ingredients):
-        # Ignore if more than 20
-        if len(raw_ingredients) > 20:
-            return False
         for elem in raw_ingredients:
-            self.ingredients = self.ingredients.append({'amount': 1, 'unit':'', 'ingredient': elem['text']}, ignore_index=True)
-        return True
+            self.ingredients.append({'amount': 1, 'unit':'', 'ingredient': elem['text']})
 
     def parse_instructions(self, raw_instructions):
-        # Ignore more than 30
-        if len(raw_instructions) > 30:
-            return False
         for elem in raw_instructions:
-            self.instructions = self.instructions.append(pd.Series(elem['text'], dtype=str), ignore_index=True)
-        return True
+            self.instructions.append(elem['text'])
     
     def get_ingredient_amounts(self, raw_ingredients):
-        for _, ingredient in self.ingredients.iterrows():
-            # Find corresponding ingredient text in raw recipe
-            for raw in raw_ingredients:
-                text = raw['text'].lower()
-                if ingredient.ingredient in text:
-                    quants = qparse.parse(text)
-                    if quants:
-                        entity = quants[0].unit.entity.name
-                        ingredient.amount = quants[0].value
-                        unit = quants[0].unit.name
-                        if unit == 'dimensionless':
-                            ingredient.unit = ''
-                        elif not entity in 'volume, mass, length':
-                            # c. parses to centavo or cent, cup cubed to cubic cup
-                            if entity == 'currency' or unit == 'cubic cup':
-                                ingredient.unit = 'cup'
-                            # pinch (Prise) parses to pint inch
-                            elif unit == 'pint inch':
-                                ingredient.unit = 'pinch'
-                            # pkg. parses to peck gram
-                            elif unit == 'peck gram':
-                                ingredient.unit = 'package'
-                            else:
-                                ingredient.unit = ''
-                        else:
-                          ingredient.unit = unit
-                    # break because unit and amount have been found
-                    break
+        i = 0
+        for ingredient in self.ingredients:
+            # Find corresponding ingredient text in raw recipe and parse
+            quants = qparse.parse(raw_ingredients[i]['text'].lower())
+            if quants:
+                entity = quants[0].unit.entity.name
+                ingredient['amount'] = quants[0].value
+                unit = quants[0].unit.name
+                if not entity in 'volume, mass, length':
+                    # c. parses to centavo or cent, cup cubed to cubic cup
+                    if 'centavo' in unit or 'cent' in unit or 'cup' in unit:
+                        ingredient['unit']= 'cup'
+                    # pinch (Prise) parses to pint inch
+                    elif unit == 'pint inch':
+                        ingredient['unit']= 'pinch'
+                    # pkg. parses to peck gram
+                    elif unit == 'peck gram':
+                        ingredient['unit']= 'package'
+                    else:
+                        ingredient['unit']= ''
+                else:
+                    ingredient['unit']= unit
+            i+=1
