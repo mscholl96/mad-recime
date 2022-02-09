d859f81eb8f9c69cee67e652c8c7963ed31673cf
Maximilian Scholl <maximilian@schollies.de>
2022-02-09 22:02:17 +0100

    #12 Crawling script

--- data/recipe1M/parser/crawler.py
+++ data/recipe1M/parser/crawler.py
@@ -1,28 +1,34 @@
-from time import process_time_ns
 from bs4 import BeautifulSoup
 import requests
+import re
+import pandas as pd
+import csv
+FILE_DIR = '/Users/maximilian/Desktop/'
 
-page = requests.get('https://www.food.com/recipe/crunchy-onion-potato-bake-479149')
-if page.status_code == 200:
-    content = page.content
-else:
-    print(f'Page returned error code: {page.status_code}')
+url_file = FILE_DIR + 'websites_8.pkl'
+out_file = FILE_DIR + 'amounts_8.csv'
 
-dom = BeautifulSoup(content, 'html.parser')
+food_com = pd.read_pickle(url_file)
 
-print(f'Website title: {dom.title.string}')
+print(f'Scraping {len(food_com)} websites...')
 
-ingredient_quantities = dom.find_all("div", {"class": "recipe-ingredients__ingredient-quantity"})
-print(len(ingredient_quantities))
-
-for quantity in ingredient_quantities:
-    sup = quantity.find('sup')
-    sub = quantity.find('sub')
-    # sup and sub are the tags of the fraction numbers, if they are None, there is no fraction inside the field
-    if sup and sub:
-        # Normal fraction, such as 1/4
-        if len(quantity.contents) == 3:
-            print(f'Amount: {sup.string}/{sub.string}')
-        # Number and fraction, such as 1 1/2
-        elif len(quantity.contents) == 4:
-            print(f'Amount: {quantity.contents[0]}{sup.string}/{sub.string}')
+i = 0
+for idx, website in food_com.iterrows():
+    try:
+        page = requests.get(website['url'], timeout=5)
+        if page.status_code == 200:
+            dom = BeautifulSoup(page.content, 'html.parser')
+            ingredient_quantities = dom.find_all("div", {"class": "recipe-ingredients__ingredient-quantity"})
+            quantities = []
+            for quantity in ingredient_quantities:
+                quantities.append(quantity.get_text())
+            with open(out_file, mode='a') as f:
+                w = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                w.writerow([idx, quantities])
+        else:
+            print(f'Page {website["url"]} returned error code: {page.status_code} for index {idx}')
+    except BaseException as e:
+        print(f'Exception occured for index {idx}: {e}')
+    i+=1
+    if i % 100 == 0:
+        print(f'Progress: {i}')
